1b4d0df5830ee0604ffc248cb5266bd36610a256
Xavih830 <xhcg2003@gmail.com>
2026-01-10 12:42:14 -0500

    listo2

---
+++ frontend/README.md
@@ -0,0 +1,2 @@
+# RanQuality
+This is an app that uses the Youtube API in order to retrieve de most recent and popular songs of certain genre.
---
+++ frontend/src/components/DoubleSelection.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { IonItem, IonList, IonSelect, IonSelectOption, SelectCustomEvent } from '@ionic/react';
+import { useCategory } from './MemoryContext';
+
+interface DataOutput {
+    handleDataChange : (category : string, limit: string, date: string) => void
+}
+
+export default function DoubleSelection(props : DataOutput) {
+
+    const { category, setCategory } = useCategory();
+    const [limit, setLimit] = useState<string | undefined>(undefined);
+    const [date, setDate] = useState<string | undefined>(undefined);
+
+    const handleChange = (e : SelectCustomEvent) => {
+        
+        const limite = Number(e.detail.value);
+
+        if (isNaN(limite)){
+            setDate(e.detail.value);
+        } else {
+            setLimit(e.detail.value);
+        }
+
+        if (category != undefined && limit != undefined && date != undefined){
+            props.handleDataChange(category, limit, date);
+            setCategory(undefined);
+            setLimit(undefined);
+            setDate(undefined);
+        }
+
+    };
+
+    const customDate = {
+        header: 'Fecha de subida',
+        subHeader: 'Selecciona la fecha desde la que quieres un top'
+    };
+
+    const customLimit = {
+        header: 'Limite',
+        subHeader: 'Selecciona el limite de datos que quieres que tenga tu top'
+    };
+
+    return (
+        <IonList>
+
+            <IonItem>
+                <IonSelect label="Límite" interfaceOptions={customLimit} interface="popover" placeholder="Selecciona uno"
+                    onIonChange={handleChange}
+                >
+                    <IonSelectOption value="10">10</IonSelectOption>
+                    <IonSelectOption value="20">25</IonSelectOption>
+                    <IonSelectOption value="50">50</IonSelectOption>
+                    <IonSelectOption value="70">75</IonSelectOption>
+                    <IonSelectOption value="100">100</IonSelectOption>
+                </IonSelect>
+            </IonItem>
+
+            <IonItem>
+                <IonSelect label="Fecha" interfaceOptions={customDate} interface="popover" placeholder="Selecciona uno"
+                    onIonChange={handleChange}
+                >
+                    <IonSelectOption value="1_semana">1 semana</IonSelectOption>
+                    <IonSelectOption value="1_mes">1 mes</IonSelectOption>
+                    <IonSelectOption value="6_meses">6 meses</IonSelectOption>
+                    <IonSelectOption value="1_año">1 año</IonSelectOption>
+                </IonSelect>
+            </IonItem>
+
+        </IonList>
+    );
+}
